89493a9eb21fc08ee5fb818d16b9804e2e8104ca
Brian Apeli <apelibrian@gmail.com>
2021-12-07 14:06:20 +0300

    intergrate firebase

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  target: "serverless",
-  async rewrites() {
-    return [
-      // Rewrite everything to `pages/index`
-      {
-        source: "/:any*",
-        destination: "/",
-      },
-    ];
-  },
-};
+  reactStrictMode: true,
+}
--- utils/firebase.js
+++ utils/firebase.js
@@ -9,3 +9,13 @@ const config = {
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId
 };
+
+//initialize firebase apps if there are no initialized apps
+if(!firebase.apps.length){
+  firebase.initializeApp(config);
+}
+//since we will be interracting with firestore, we
+//grab a refference to the firestore database object and export it from this file 
+const firestore = firebase.firestore();
+
+export{firestore};
